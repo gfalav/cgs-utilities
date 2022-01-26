7567b439e478d50160d1f8a12abb68d9dfc539a5
gfalav <gfalav@hotmail.com>
2022-01-26 16:12:02 -0300

    with sidebar y topbar markup

--- src/comps/layout/Layout.jsx
+++ src/comps/layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { AppBar, Box, Divider, Drawer, IconButton, Toolbar, useMediaQuery } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu'
-import SideMenu from './sidebar/SideMenu';
-import LogoMark from './sidebar/LogoMark';
+import { Box, Divider, Drawer, useMediaQuery } from "@mui/material"
+import SideMenu from './sidebar/SideMenu'
+import LogoMark from './sidebar/LogoMark'
+import Topbar from './topbar/Topbar'
 
 const Layout = () => {
     const isMobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
-    const drawerWidth = 300
+    const drawerWidth = 240
     const [toggleMenu, setToggleMenu] = React.useState(false)
 
     const DrawerIn = () => {
@@ -20,28 +20,10 @@ const Layout = () => {
         )
     }
 
+
     return(
         <Box sx={{display: 'flex'}}>
-            <AppBar position="fixed" 
-                    sx={{
-                        width: { sm: `calc(100% - ${drawerWidth}px)` },
-                        ml: { sm: `${drawerWidth}px` },
-                      }}
-            >
-                <Toolbar>
-                    <IconButton 
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={() => setToggleMenu(!toggleMenu)}
-                        sx={{ mr: 2, display: {sm: 'none'} }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    
-                </Toolbar>
-            </AppBar>
+            <Topbar drawerWidth={drawerWidth} toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
             { !isMobile && <Drawer 
                                 variant='persistent'
                                 anchor="left"
---
+++ src/comps/layout/topbar/Topbar.jsx
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import { AppBar, Badge, Box, IconButton, Menu, MenuItem, Toolbar } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import MailIcon from '@mui/icons-material/Mail'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+
+
+const Topbar = (drawerWidth, toggleMenu, setToggleMenu) => {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    return(
+        <AppBar position="fixed" 
+                sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` },
+                    backgroundColor: '#ebebeb'                       
+                  }}
+        >
+            <Toolbar>
+                <IconButton 
+                    size="large"
+                    edge="start"
+                    color="primary"
+                    aria-label="menu"
+                    onClick={() => setToggleMenu(!toggleMenu)}
+                    sx={{ mr: 2, display: {sm: 'none'} }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Box sx={{flexGrow: 1}} />
+                <IconButton sx={{mr: 1}}>
+                    <Badge badgeContent={10} color="secondary">
+                        <NotificationsIcon color="primary" />
+                    </Badge>
+                </IconButton>
+                <IconButton sx={{mr: 1}}>
+                    <Badge badgeContent={4} color="secondary">
+                        <MailIcon color="primary" />
+                    </Badge>
+                </IconButton>
+                <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}>
+                  <AccountCircleIcon color="primary" fontSize='large' />
+                </IconButton>
+                <Menu 
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    >
+                    <MenuItem onClick={handleClose}>SignIn</MenuItem>
+                    <MenuItem onClick={handleClose}>SignUp</MenuItem>
+                </Menu>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Topbar
